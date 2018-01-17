a81e24fa7b20679a64b14a11be9128a625f2c40b
rdrew <robdrewupei@gmail.com>
2018-01-17 14:02:25 -0400

    added togulp file

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,29 +4,53 @@ import yaml from 'js-yaml';
 import plugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
 import sassGlob from 'gulp-sass-glob';
+import imagemin from 'gulp-imagemin';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
 
-
-// Load settings from settings.yml
-const {
-    REGEX,
-    URL,
-    PORT,
-    PATHS
-} = loadConfig();
-
+// Import settings in yml format from external config file (gulp.settings.yml)
 function loadConfig() {
     let ymlFile = fs.readFileSync('gulp.settings.yml', 'utf8');
     return yaml.load(ymlFile);
 }
 
+// Load variables from gulp.settings.yml
+const { REGEX, URL, PORT, PATHS } = loadConfig();
 
 /*
- *Task: BrowserSync Remote Proxy
+ * Primary Gulp Tasks
  */
-gulp.task('bsRemote', ['sass'], function() {//{{{
+
+// Default Task
+gulp.task('default', ['sass-dev', 'images', 'bsRemote'], function(){
+    // watch and compile sass
+    gulp.watch( PATHS.sassWatch, ['sass-dev']);
+    // watch for minify images
+    gulp.watch( PATHS.images.source, ['images']);
+    // watch this stuff and reload the browser when there are changes
+    gulp.watch( PATHS.reloadFiles ).on('change', browserSync.reload);
+});
+
+// 'build' task (for production: compressed css, no sourcemaps etc)
+gulp.task('build', ['images', 'sass-prod']);
+
+/*
+ * Subtasks
+ */
+
+ //sub-task: Imagemin
+gulp.task('images', function() {
+//{{{
+    gulp.src(PATHS.images.source)
+        .pipe(imagemin())
+        .pipe(gulp.dest(PATHS.images.destination))
+
+});//}}}
+
+ //sub-task: BrowserSync Remote Proxy
+gulp.task('bsRemote', function() {
+//{{{
     var stringRemove = new RegExp( REGEX, 'g');
     browserSync.init({
         proxy: URL,
@@ -35,28 +59,38 @@ gulp.task('bsRemote', ['sass'], function() {//{{{
         injectChanges: true,
         files: PATHS.mainFiles,
         rewriteRules: [{
-
             match: stringRemove,
             fn: function(matched) {
                 return ''
-
             }
-
         }]
-
     });
-    gulp.watch( PATHS.sassWatch, ['sass']);
-    gulp.watch( PATHS.reloadFiles ).on('change', browserSync.reload);
 });//}}}
 
-
-/*
- *Task: Sass compiler
- */
-gulp.task('sass', function() {//{{{
+ //sub-task: Sass compiler (dev)
+gulp.task('sass-dev', function() {
+//{{{
     return gulp.src('scss/app.scss')
         .pipe(sassGlob())
         .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            includePaths: PATHS.sass_includes
+            //outputStyle: 'compressed' // if css compressed **file size**
+        })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9']
+        }))
+        .pipe($.sourcemaps.write())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
+});//}}}
+
+ //sub-task: Sass compiler (prod)
+gulp.task('sass-prod', function() {
+//{{{
+    return gulp.src('scss/app.scss')
+        .pipe(sassGlob())
         .pipe($.sass({
             includePaths: PATHS.sass_includes,
             outputStyle: 'compressed' // if css compressed **file size**
@@ -66,8 +100,6 @@ gulp.task('sass', function() {//{{{
             browsers: ['last 2 versions', 'ie >= 9']
         }))
         .pipe(gulp.dest('css'))
-        .pipe(browserSync.stream());
 });//}}}
 
 
-gulp.task('default', ['bsRemote']);
