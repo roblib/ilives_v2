1643474897d6d68bd641fa38444c06c5d58e4eb7
rdrew <robdrewupei@gmail.com>
2018-01-18 15:37:17 -0400

    .

--- node_modules/foundation-sites/node_modules/foundation-docs/helpers/raw.js
+++
@@ -1,6 +0,0 @@
-/**
- * This helper is used to render documentation pages that have Handlebars code. The raw helper ignores any Handlebars code inside of it.
- */
-module.exports = function(content) {
-  return content.fn();
-}
--- node_modules/foundation-sites/node_modules/foundation-docs/lib/handlebars/partials.js
+++
@@ -1,9 +0,0 @@
-var fs         = require('fs');
-var handlebars = require('handlebars');
-var path       = require('path');
-
-var PARTIALS = ['building-blocks', 'header', 'javascript-reference', 'sass-reference', 'table-of-contents', 'video-hero-header'];
-
-for (var i in PARTIALS) {
-  handlebars.registerPartial(PARTIALS[i], fs.readFileSync(path.join(__dirname, '../../templates/partials/'+PARTIALS[i]+'.hbs')).toString());
-}
