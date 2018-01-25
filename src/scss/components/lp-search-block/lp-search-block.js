a241b288cf65ab4c22152634b05d462c9d12aad8
rdrew <robdrewupei@gmail.com>
2018-01-25 13:59:01 -0400

    .

---
+++ src/scss/components/lp-search-block/lp-search-block.js
@@ -0,0 +1,11 @@
+
+/*
+Drupal.behaviors.lp-search-block = {
+  attach: function (context, settings) {
+    $('#some_element', context).once('lp-search-block', function () {
+      // Code here will only be applied to $('#some_element')
+      // a single time.
+    });
+  }
+};
+*/
