9907b8c820007e9f668183296ceab97b4ddbee55
Rob Drew <rdrew@upei.ca>
2018-02-05 16:15:26 -0400

    page template

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -40,28 +40,7 @@ $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL));
 
 <div class="page">
   <div role="main" id="main-content">
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-    <h1><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
 
-    <?php print $breadcrumb; ?>
-
-    <?php if ($action_links): ?>
-    <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
-
-    <?php if (isset($tabs['#primary'][0]) || isset($tabs['#secondary'][0])): ?>
-    <nav class="admin_tabs"><?php print render($tabs); ?></nav>
-    <?php endif; ?>
-
-    <?php if($page['highlighted'] OR $messages){ ?>
-    <div class="drupal-messages">
-      <?php print render($page['highlighted']); ?>
-      <?php print $messages; ?>
-    </div>
-    <?php } ?>
     <?php print render($page['content_pre']); ?>
 
     <?php print render($page['content']); ?>
@@ -70,17 +49,6 @@ $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL));
 
   </div><!-- /main-->
 
-  <?php if ($page['sidebar_first']): ?>
-  <div class="sidebar-first">
-    <?php print render($page['sidebar_first']); ?>
-  </div>
-  <?php endif; ?>
-
-  <?php if ($page['sidebar_second']): ?>
-  <div class="sidebar-second">
-    <?php print render($page['sidebar_second']); ?>
-  </div>
-  <?php endif; ?>
 </div><!-- /page-->
 
 <footer role="contentinfo">
