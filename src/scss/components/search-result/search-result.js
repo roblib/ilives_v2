a6ae5899908cedecb8c271a54e172f85d1c218ed
rdrew <robdrewupei@gmail.com>
2018-02-08 16:14:05 -0400

    search results

---
+++ src/scss/components/search-result/search-result.js
@@ -0,0 +1,11 @@
+
+/*
+Drupal.behaviors.search-result = {
+  attach: function (context, settings) {
+    $('#some_element', context).once('search-result', function () {
+      // Code here will only be applied to $('#some_element')
+      // a single time.
+    });
+  }
+};
+*/
