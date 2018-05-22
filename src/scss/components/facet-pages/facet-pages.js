779c18e9fb4cf983340bc9fa678f1a6cd1310ddb
rdrew <robdrewupei@gmail.com>
2018-05-22 14:25:31 -0300

    facet pages update

---
+++ src/scss/components/facet-pages/facet-pages.js
@@ -0,0 +1,11 @@
+
+/*
+Drupal.behaviors.facet-pages = {
+  attach: function (context, settings) {
+    $('#some_element', context).once('facet-pages', function () {
+      // Code here will only be applied to $('#some_element')
+      // a single time.
+    });
+  }
+};
+*/
